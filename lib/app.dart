c4b28a92819f2af23e89475488f3b3e18e43c2c9
austhedze <thedzengwaaustin@gmail.com>
2025-10-15 13:47:44 +0200

    final commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,30 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'screens/home_screen.dart';
-
-// class WeatherTrackerApp extends StatelessWidget {
-//   const WeatherTrackerApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Weather Tracker',
-//       theme: ThemeData(
-//         useMaterial3: true,
-//         colorScheme: ColorScheme.fromSeed(
-//           seedColor: Colors.blue,
-//           brightness: Brightness.dark,
-//         ),
-//         textTheme: GoogleFonts.poppinsTextTheme(
-//           Theme.of(context).textTheme,
-//         ),
-//       ),
-//       home: const HomeScreen(),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
-
 
 
 import 'package:flutter/material.dart';
